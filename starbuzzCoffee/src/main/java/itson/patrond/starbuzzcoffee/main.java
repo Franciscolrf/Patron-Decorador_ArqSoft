438d29fa56ce44f647621df8c613a6dd7fd2b17c
Francisco LR <francisco.lopez247037@potros.itson.edu.mx>
2024-09-07 16:28:04 -0700

    Clases y paquetes
    Se crearon las clases y paquetes del  proyecto.

--- starbuzzCoffee/src/main/java/itson/patrond/starbuzzcoffee/main.java
+++ starbuzzCoffee/src/main/java/itson/patrond/starbuzzcoffee/main.java
@@ -6,9 +6,10 @@ package itson.patrond.starbuzzcoffee;
 
 /**
  *
- * @author Fran
+ * @author Francisco de Jesús López Ruiz - 00000247037
+ * Instituto Tecnológico de Sonora (ITSON)
  */
-public class StarbuzzCoffee {
+public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
