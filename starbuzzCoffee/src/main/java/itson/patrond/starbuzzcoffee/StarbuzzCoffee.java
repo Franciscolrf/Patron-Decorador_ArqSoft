36bac2768c7b9d30f4b1b81e49892124fbc3ee8b
Francisco LR <francisco.lopez247037@potros.itson.edu.mx>
2024-09-07 16:07:00 -0700

    Creación
    Se creó el proyecto

---
+++ starbuzzCoffee/src/main/java/itson/patrond/starbuzzcoffee/StarbuzzCoffee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.patrond.starbuzzcoffee;
+
+/**
+ *
+ * @author Fran
+ */
+public class StarbuzzCoffee {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
