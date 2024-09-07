e8196aafda00208ff0aa1e009a1906767dd6377d
Francisco LR <francisco.lopez247037@potros.itson.edu.mx>
2024-09-07 16:41:14 -0700

    Clase decaf
    decaf implementado

--- starbuzzCoffee/src/main/java/itson/patrond/starbuzzcoffee/bebidas/Decaf.java
+++ starbuzzCoffee/src/main/java/itson/patrond/starbuzzcoffee/bebidas/Decaf.java
@@ -5,9 +5,26 @@
 package itson.patrond.starbuzzcoffee.bebidas;
 
 /**
- *
- * @author Fran
+ * Clase que representa una bebida Decaf
+ * @author Francisco de Jesús López Ruiz - 00000247037
+ * Instituto Tecnológico de Sonora (ITSON)
  */
-public class Decaf {
+public class Decaf extends Beverage {
+    
+    /**
+     * Constructor de la clase
+     */
+    public Decaf(){
+        description = "Cafe Decaf";
+    }
+    
+    /**
+     * Calcula el costo de la bebida Decaf
+     * @return Costo de la bebida
+     */
+    @Override
+    public double cost() {
+        return 30.00;
+    }
     
 }
